bb13cd80f864643cf9556364dc2deeec9f254aec
Simone Peer <simone.peer@gmail.com>
2019-05-17 11:22:51 +0100

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
 require 'open-uri'
 
 Ingredient.destroy_all
 
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-user_serialized = open(url).read
-cocktails = JSON.parse(user_serialized)
+puts 'Creating cocktails...'
 
-cocktails['drinks'].each do |ingredient|
-  Ingredient.create!(name: ingredient['strIngredient1'])
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+response = JSON.parse(open(url).read)
+
+response['drinks'].each do |x|
+  Ingredient.create(name: x['strIngredient1'])
 end
+
+Cocktail.destroy_all
+
+cocktail_attributes = [
+  {
+    name: 'Martini'
+  },
+  {
+    name: 'Manhattan'
+  },
+  {
+    name: 'Bloody Mary'
+  },
+  {
+    name: 'Margarita'
+  },
+  {
+    name: 'Old Fashioned'
+  },
+  {
+    name: 'Mojito'
+  },
+  {
+    name: 'Daquiri'
+  },
+  {
+    name: 'Gin Tonic'
+  }
+]
+Cocktail.create!(cocktail_attributes)
+
+puts 'Finished!'
